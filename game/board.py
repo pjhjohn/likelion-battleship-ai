d90a89b9991302ce5e5abb44ba5f6651643ba373
Alex Han <alexhan46@gmail.com>
2014-08-01 21:01:15 +0900

    all logs and guess error messages deleted

--- game/board.py
+++ game/board.py
@@ -17,30 +17,21 @@ class Board:
                 else:
                     self.board[ coordinate['y'] ][ coordinate['x'] ] = battleship.ship_id # change occupied tile to 1
 
-    def print_battleships(self):
-        for battleship in self.battleships:
-            print "ID: %d, Size: %d, Health: %d" %(battleship.ship_id, battleship.size, battleship.health)
-
     def hit( self, player, guess ):
         """
         check if guess hit
         guess : (dic) guess location
         """
         if ( guess['x'] > 9 or guess['x'] < 0 ) or ( guess['y'] > 9 or guess['y'] < 0 ):
-            print "Player%d, invalid guess (%d,%d)" %(player,guess['x'],guess['y'])
-            # raise ValueError("Your guess (%d,%d) is out of the board." %(guess['x'],guess['y']))
             return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":-2, "sink":None}
 
         else:
             coordinate = self.board[ guess['y'] ][ guess['x'] ]
             if coordinate < 0:
-                print "Player%d, guessed already (%d,%d)" %(player,guess['x'],guess['y'])
-                # raise ValueError("You already guessed (%d,%d)." %(guess['x'],guess['y'])) 
                 return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":-1, "sink":None}
 
             elif coordinate == 0:
                 self.board[ guess['y'] ][ guess['x'] ] = -2
-                print "Player%d, missed (%d,%d)" %(player,guess['x'],guess['y'])
                 return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":0, "sink":None}
 
             elif coordinate > 0 :
@@ -48,20 +39,12 @@ class Board:
 
                 self.board[ guess['y'] ][ guess['x'] ] = -1
                 self.remaining -= 1
-                print 'Hit Ship ID %d' %(hit_ship_id)
                 self.battleships[ ( hit_ship_id - 1 ) ].health -= 1
-                self.print_battleships()
-                print 'Hit Ship Health : %d' %self.battleships[(hit_ship_id-1)].health
                 if self.battleships[ ( hit_ship_id - 1 ) ].health == 0:
-                    print "Player%d, Ship size %d has sank. (%d,%d)" % (player, self.battleships[ ( hit_ship_id - 1) ].size, guess['x'], guess['y'])
                     if self.remaining == 0:
                         return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":3, "sink":hit_ship_id}
                     else:
                         return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":2, "sink":hit_ship_id}
 
                 else:
-                    print "Player%d, Hit Ship%d! (%d, %d)" %(player, hit_ship_id, guess['x'], guess['y'])
                     return {"player":player, "guess":{"x":guess['x'],"y":guess['y']}, "result":1, "sink":None}
-
-            else:
-                print "WHAT?!"
--- game/game.py
+++ game/game.py
@@ -17,12 +17,6 @@ constant.RESULT_HIT     = 1
 constant.RESULT_SINK    = 2
 constant.RESULT_WIN     = 3
 
-battleships1 = [{"location":{"x":1,"y":1},"size":2,"direction":"x"},{"location":{"x":4,"y":2},"size":4,"direction":"y"},{"location":{"x":7,"y":3},"size":3,"direction":"x"},{"location":{"x":2,"y":4},"size":3,"direction":"y"},{"location":{"x":5,"y":7},"size":5,"direction":"x"}]
-battleships2 = [{"location":{"x":1,"y":1},"size":2,"direction":"x"},{"location":{"x":1,"y":4},"size":4,"direction":"x"},{"location":{"x":1,"y":3},"size":3,"direction":"x"},{"location":{"x":1,"y":2},"size":3,"direction":"x"},{"location":{"x":5,"y":9},"size":5,"direction":"x"}]
-
-ai1 = "ai"
-ai2 = "ai_test"
-
 def make_fleet(battleships):
     battleships.sort(key=operator.itemgetter("size"))
     
@@ -52,15 +46,6 @@ def make_fleet(battleships):
                 raise ValueError("There are more than one size %d battleship." %bs["size"])
             fleet.append(Battleship(bs["size"],bs["direction"],dict(bs["location"]), bs["size"]))
     return fleet
-
-def print_board(board):
-    for row in board:
-        for col in row:
-            if col >= 0:
-                print " ",col,
-            else:
-                print "",col,
-        print "\n"
 
 def convert_board(board):
 
@@ -114,70 +99,32 @@ def game( battleships1, battleships2, ai1, ai2 ):
 
         while hit1 > 0:
             turn1 += 1
-            print "Turn %d" %turn1
             guess1 = {}
             guess1['x'], guess1['y'] = ai1.guess(results1)
-            # guess1_tuple = ai1.guess(results2)
-            # guess1["x"] = guess1_tuple[0]
-            # guess1["y"] = guess1_tuple[1]
             last_result = gb1.hit(1, guess1)
             log.history.append(last_result)
             results1.update_board(convert_board(gb1.board)) 
-            print last_result
-            print_board(gb1.board)
             results1.history.append({"guess":last_result["guess"],"result":last_result["result"],"sink":last_result["sink"]})
             hit1 = last_result["result"]
             if hit1 == 3:
                 break
-            print ""
 
-        if hit1 == 3:
-            print "Player1 Won!"
-            break
-        elif hit1 == -1:
-            print "Player1 Lost! (Player1 hit the same location twice)"
-            break
-        elif hit1 == -2:
-            print "Player1 Lost! (Player1 hit invalid location)"
+        if hit1 == 3 or hit1 == -1 or hit1 == -2:
             break
 
         while hit2 > 0:
             turn2 += 1
-            print "Turn %d" %turn2
             guess2 = {}
             guess2['x'], guess2['y'] = ai2.guess(results2)
-            # guess2_tuple = ai2.guess(results2)
-            # guess2["x"] = guess2_tuple(0)
-            # guess2["y"] = guess2_tuple(1)
             last_result = gb2.hit(2, guess2)
             log.history.append(last_result)
             results2.update_board(convert_board(gb2.board))
-            print last_result
-            print_board(gb2.board)
             results2.history.append({"guess":last_result["guess"],"result":last_result["result"],"sink":last_result["sink"]})
             hit2 = last_result["result"]
             if hit2 == 3:
                 break
-            print ""
 
-        if hit2 == 3:
-            print "Player2 Won!"
+        if hit2 == 3 or hit2 == -1 or hit2 == -2:
             break
-        elif hit2 == -1:
-            print "Player2 Lost! (Player2 hit the same location twice)"
-            break
-        elif hit2 == -2:
-            print "Player2 Lost! (Player2 hit invalid location)" 
-            break
-
-    print_board(gb1.board)
-    print results1.history
-    print ""
-    print_board(gb2.board)
-    print results2.history
-    print log.history
 
     return log.get_log()
-    
-print game(battleships1, battleships2, ai1, ai2)
-print game(battleships1, battleships2, ai1, ai2)
